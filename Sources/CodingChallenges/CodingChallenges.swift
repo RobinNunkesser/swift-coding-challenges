d6c10e5aded18f0c16013d462133c5f5227cb5d8
nunkesser <robin.nunkesser@hshl.de>
2023-07-02 20:39:08 +0200

    Initial Commit

---
+++ Sources/CodingChallenges/CodingChallenges.swift
@@ -0,0 +1,6 @@
+public struct CodingChallenges {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/CodingChallengesTests/CodingChallengesTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CodingChallenges
+
+final class CodingChallengesTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CodingChallenges().text, "Hello, World!")
+    }
+}
